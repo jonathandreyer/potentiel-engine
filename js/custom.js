a3a4dd2eec689c4090b361ddb24c04320fac8847
Jonathan Dreyer <jonathan.dreyer@cleanenergie.ch>
2019-02-18 22:22:04 +0100

    Update sources

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,15 @@
 
 	$(document).ready(function() {
 
+		$('#submit').on('click', function(event) {
+		  event.preventDefault(); // To prevent following the link (optional)
+			var radioValue = $("input[name='choice']:checked").val();
+			ga('send', {
+			  hitType: 'event',
+			  eventCategory: 'Submit'
+			});
+		});
+
 	});
 
 })(jQuery);
