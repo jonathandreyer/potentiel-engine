a461f27cbc2e58e0c6f7e6f8bccf3ddeade27ba6
Jonathan Dreyer <jonathan.dreyer@cleanenergie.ch>
2019-02-20 22:26:06 +0100

    Remove snippet GA

--- js/custom.js
+++ js/custom.js
@@ -5,10 +5,6 @@
 		$('#submit').on('click', function(event) {
 		  event.preventDefault(); // To prevent following the link (optional)
 			var radioValue = $("input[name='choice']:checked").val();
-			ga('send', {
-			  hitType: 'event',
-			  eventCategory: 'survey'
-			});
 			window.dataLayer.push({
 				'event': 'survey',
 				'value': radioValue
